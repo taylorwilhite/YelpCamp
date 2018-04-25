ad7824e102d66e2b6a3c2b29f64e5aab2c47e771
Taylor Wilhite <taylorwilhite@gmail.com>
2018-04-24 19:29:06 -0700

    link campground to creating user and display on show page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,12 +15,16 @@ router.get("/", function(req, res){
 });
 
 //CREATE: add campgrounds
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
 	//get data from form and add
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newCampground = {name: name, image: image, description: desc};
+	var author = {
+		id: req.user._id,
+		username = req.user.username
+	};
+	var newCampground = {name: name, image: image, description: desc, author: author};
 	//create new campground and save to db
 	Campground.create(newCampground, function(err, newlyCreated){
 		if(err){
@@ -32,7 +36,7 @@ router.post("/", function(req, res){
 });
 
 //NEW: show form to create campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
 	res.render("campgrounds/new");
 });
 
@@ -48,5 +52,12 @@ router.get("/:id", function(req, res){
 		}
 	});
 });
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+};
 
 module.exports = router;
