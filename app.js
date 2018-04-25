f4be1ad5a1e0e149032366e0609991a4af252181
Taylor Wilhite <taylorwilhite@gmail.com>
2018-04-24 19:15:21 -0700

    remove seedDB from runtime, save comments to loggedin user and display username with comments

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var indexRoutes = require("./routes/index");
 
 
 //set options
-seedDB();
+// seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
